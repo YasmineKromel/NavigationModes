bd359d5f846afe168df37a7a81d738ec6c5fa3c5
yasmine Soubhi kromel <yasmine@appstudio.pro>
2019-06-21 17:53:02 +0200

    Initial Commit

---
+++ NavigateModes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigateModes
+//
+//  Created by Yasmine on 6/21/19.
+//  Copyright © 2019 Yasmine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
